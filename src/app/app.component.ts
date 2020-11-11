b1a1b2a49facc5c9491af0b8238cf45758c0cc69
Rajat Khandelwal <rajatkhandelwal094@gmail.com>
2020-11-11 14:53:05 +0530

    Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { FormControl, Validators } from "@angular/forms";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'train-man';
+  title = "train-man";
+  progressInputValue: number = 1;
+
+  startFlag: boolean = false;
+  private _timeRemainingPer: number = 0;
+
+  timeLeftInSec: number = 0;
+  schdeular: any;
+
+  get timeRemainingPer(): number {
+    return this._timeRemainingPer;
+  }
+
+  set timeRemainingPer(value: number) {
+    if (!isNaN(value) && value <= 100) {
+      this._timeRemainingPer = value;
+    }
+  }
+
+  constructor() {}
+
+  onStart() {
+    this.startFlag = true;
+    if (isNaN(this.progressInputValue) || this.progressInputValue == null) {
+      this.startFlag = false;
+      return;
+    }
+    this.timeLeftInSec = this.progressInputValue * 60;
+
+    this.schdeular = setInterval(() => {
+      if (this.timeLeftInSec > 0) {
+        this.timeLeftInSec = this.timeLeftInSec - 1;
+        this.timeRemainingPer =
+          (this.timeLeftInSec / (this.progressInputValue * 60)) * 100;
+      } else {
+        this.onReset();
+      }
+    }, 1000);
+  }
+
+  onStop() {
+    this.startFlag = false;
+    clearInterval(this.schdeular);
+  }
+
+  onReset() {
+    this.startFlag = false;
+    clearInterval(this.schdeular);
+    this.timeLeftInSec = this.progressInputValue * 60;
+    this.timeRemainingPer = 0;
+  }
+
+  getMyTime() {
+    const myTime = new Date(1970, 0, 1);
+    myTime.setSeconds(this.timeLeftInSec);
+    return myTime;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule } from "@angular/material";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatButtonModule,
+    FormsModule,
+    MatProgressBarModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
